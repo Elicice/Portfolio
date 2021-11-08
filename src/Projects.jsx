96af8f25e28a74ef483836320ed53a10e67665e8
Cecilie Lyshoel <cecilie@lyshoel.com>
2021-11-08 20:01:33 +0100

    Added h1 header

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -10,26 +10,27 @@ import Typography from '@mui/material/Typography';
 
 export default function Projects() {
   return (
+    
+    <><h1>Projects</h1>
     <Card sx={{ maxWidth: 345 }}>
       <CardMedia
         component="img"
         alt="Binary clock"
         height="140"
-        image="./BinaryClock.png"
-      />
+        image="./BinaryClock.png" />
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
           Binary Clock
         </Typography>
         <Typography variant="body2" color="text.secondary">
           A binary clock to show the time in a binary way.
-          
+
         </Typography>
       </CardContent>
       <CardActions>
         <Button size="small" href="/#/Projects/BinaryClock">Demo</Button>
         <Button size="small" href="https://github.com/Elicice/Portfolio/tree/main/src/projects/klokke-app">Source</Button>
       </CardActions>
-    </Card>
+    </Card></>
   );
 }
