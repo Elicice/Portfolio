634f63beccc0ee0a6ef0d45bcd2f6083b5a511a7
Cecilie Lyshoel <cecilie@lyshoel.com>
2021-11-08 19:59:53 +0100

    Added styling

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,13 +1,14 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
+import style from './HomePage.css'
 
 export default function HomePage() {
   return (
     
     <Box
       sx={{
-        width: '60%',
+        width: '85%',
         marginTop: '20px',
         display: 'flex',
         flexWrap: 'wrap',
@@ -17,7 +18,7 @@ export default function HomePage() {
           height: '85vh',
           width: '100%',
           alignItems: 'center',
-          padding: '20px',
+          padding: '10px',
 
         },
       }}
@@ -31,7 +32,20 @@ export default function HomePage() {
         
         }}>
             <h1>Cecilie Lyshoel</h1>
-           is a University of Bergen Alumni...
+             <section className="sectionmrl">
+                <img src="/./kaffekopp 4 crop.jpg" alt="Cup of coffee on stacked books, next to a rainy window." className="imageHome" />
+              </section>
+              <section className="sectionmrl">
+                I am a University of Bergen alumni with degrees in education and information science.
+              </section>
+              
+              <section className="sectionmrl">
+                Learning, being creative, and working with technology are some things I enjoy doing. 
+              </section>
+              <section className="sectionmrl">
+                <img src="/./kaffebønner hjerte crop.jpg" alt="Kaffebønner i form som et hjerte" className="imageHome"/>
+              </section>
+            
      </Paper>
 
     </Box>
